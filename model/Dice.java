f7d3a23f3632c36d49e8457bf27c2956604735cd
Devin Dinh <devind1@arizona.edu>
2024-11-19 08:27:03 -0700

    Implemented Dice as a flyweight object along with an Enum for Dice values

--- model/Dice.java
+++ model/Dice.java
@@ -2,42 +2,43 @@
  * Juan Rogel Acedo (jarogelacedo)
  * Name (netid)
  * Name (netid)
- * Name (netid)
+ * Devin Dinh (devdinh)
  */
+
 import java.util.Random;
 
-public class Dice{
-    private int value;
-    private boolean isHeld;
-    private static final Random random = new Random();
+// Flyweight implementation of Dice
+public class Dice {
+    private DiceValue face;
+    private Random rand;
     
-    public Dice(){
-        this.value = 1;
-        this.isHeld = false;
+    // private constructor
+    private Dice(DiceValue face) {
+        this.face = face;
     }
 
-    public void roll(){
-        this.value = random.nextInt(6) + 1;
+    // static store
+    private static Dice[] diceArr = new Dice[5];
+    
+    static {
+        for (int i = 0; i < diceArr.length; i++) {
+            diceArr[i] = new Dice(DiceValue.values()[i]);
+        }
     }
 
-    /*
-     * @return the value of the dice
-     */
-    public int getValue(){
-        return this.value;
+    // static access method
+    public static Dice get(int dicePos) {
+        assert dicePos > 0 && dicePos < 6;
+        return diceArr[dicePos];
+    }
+    
+    public void roll() {
+        int randRoll = rand.nextInt(7);
+        this.face = DiceValue.values()[randRoll];
     }
 
-    /*
-     * @return true if the dice is held, false otherwise
-     */
-    public boolean isHeld(){
-        return this.isHeld;
-    }   
-
-    /*
-     * @param hold true if the dice is held, false otherwise
-     */
-    public void setHeld(boolean hold){
-        this.isHeld = hold;
+    public DiceValue getFace() {
+        return this.face;
     }
+    
 }
---
+++ model/DiceValue.java
@@ -0,0 +1,10 @@
+/*
+ * Juan Rogel Acedo (jarogelacedo)
+ * Name (netid)
+ * Name (netid)
+ * Devin Dinh (devdinh)
+ */
+
+public enum DiceValue {
+    ONE, TWO, THREE, FOUR, FIVE, SIX;
+}
